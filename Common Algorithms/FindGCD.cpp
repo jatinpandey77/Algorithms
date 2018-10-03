f4db17c2076841aa374f5f3b126c573f34eaf4d5
Trijeet Ganguly <trijeet.ganguly@gmail.com>
2018-10-03 21:48:23 +0530

    Update FindGCD.cpp
    Extended range ;)

--- Common Algorithms/FindGCD.cpp
+++ Common Algorithms/FindGCD.cpp
@@ -2,14 +2,17 @@
 // C++ program to find GCD of two numbers
 #include <iostream>
 
+#define int long long
 // Recursive function to return gcd of a and b
 int gcd(int a, int b){
     if (b == 0)
         return a;
     return gcd(b, a % b);
 }
- 
+
+#undef int
 int main(){
+#define int long long
     int a, b;
 	cin>>a>>b;
     cout<<"GCD of "<<a<<"and "<<b<<"is : "<<gcd(a, b)<<endl;
